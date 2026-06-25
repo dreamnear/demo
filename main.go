3731d48794188079b212733c67b6a3c20e2cf0e7
王现甫 <wangxf@lizu.net.cn>
2026-06-25 15:00:03 +0800

    [ga:writer] task-003 attempt=1

--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"demo/course"
 	"log"
 	"net/http"
+	"os"
+
+	// Database drivers - registered via init()
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	_ "modernc.org/sqlite"
 )
 
 func main() {
+	driver := os.Getenv("DB_DRIVER")
+	if driver == "" {
+		driver = "sqlite"
+	}
+
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		dsn = "file:demo.db?_pragma=journal_mode(WAL)"
+	}
+
+	store, err := course.Open(driver, dsn)
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer store.Close()
+
+	if err := store.InitDB(); err != nil {
+		log.Fatalf("failed to initialize database: %v", err)
+	}
+
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
+
+	handler := course.NewHandler(store)
+	handler.RegisterRoutes(mux)
+
 	log.Println("listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
